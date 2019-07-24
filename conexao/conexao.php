75150554b5bec15eb815237a89061ca1b3058624
Anderson Gabriel de Souza <contatogabrielsouza23@gmail.com>
2019-07-24 17:18:39 -0300

    Update conexao.php
    edit

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -3,16 +3,5 @@
 	$usuario = "root";
 	$senha = "";
 	$dbname = "look";
-	Gabriel132
-1023129a
-gabriel12s
-agencia:1601
-conta:029.013-2
-
-
-ilda11
-
-
-00029013-2
 	//Criar a conexao
 	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
